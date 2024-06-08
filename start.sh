b8929e86678095053bd45796006574c4e983c17d
Abdur Rofi <rofim58@gmail.com>
2024-06-09 05:54:47 +0700

    update

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-sudo chmod go-w ./services/filebeat/filebeat.yml
-sudo chmod -R go-w ./services/filebeat/modules.d
-docker compose -f docker-compose.dvwa.yml down
-docker compose -f docker-compose.yml --env-file .env down
-docker compose -f docker-compose.dvwa.yml up -d
-docker compose -f docker-compose.yml --env-file .env up -d
+docker compose -f docker-compose.dvwa.yml restart -d
+docker compose -f docker-compose.yml --env-file .env restart -d
