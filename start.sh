cabb3261f1ca43c85f623c891e8c8091860c8cda
Abdur Rofi <rofim58@gmail.com>
2024-06-10 16:37:37 +0700

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker compose -f docker-compose.dvwa.yml restart -d
-docker compose -f docker-compose.yml --env-file .env restart -d
+docker compose -f docker-compose.dvwa.yml up -d
+docker compose -f docker-compose.yml --env-file .env up -d
